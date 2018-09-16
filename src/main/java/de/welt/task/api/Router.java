8549480457c4409a3236b4de59ea5facaadb1f82
spyros87 <papadopoulos.spyridon@gmail.com>
2018-09-16 18:53:59 +0200

    Add incoming request logger.

--- src/main/java/de/welt/task/api/Router.java
+++ src/main/java/de/welt/task/api/Router.java
@@ -1,7 +1,10 @@
 package de.welt.task.api;
 
 import de.welt.task.utilities.JsonSupport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static spark.Spark.before;
 import static spark.Spark.get;
 
 /**
@@ -9,7 +12,11 @@ import static spark.Spark.get;
  */
 final class Router {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Router.class);
+
     void httpRoutes() {
+
+        before((request, response) -> LOG.info("Request for [{}]", request.uri()));
 
         get("/aggregations/:id", new ApplicationHandler(), JsonSupport.jsonTransformer());
     }
