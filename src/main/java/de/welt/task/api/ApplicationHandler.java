0473d3e64e84cc02be2ea60ac8913dbbfa94cdc9
spyros87 <papadopoulos.spyridon@gmail.com>
2018-09-16 18:59:30 +0200

    Implementation of async calls.

--- src/main/java/de/welt/task/api/ApplicationHandler.java
+++ src/main/java/de/welt/task/api/ApplicationHandler.java
@@ -1,16 +1,23 @@
 package de.welt.task.api;
 
+import com.spotify.futures.CompletableFutures;
 import de.welt.task.domain.ResponseContainer;
+import de.welt.task.domain.User;
+import de.welt.task.domain.UserPost;
+import de.welt.task.utilities.JsonSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.function.Supplier;
 
 import static com.google.common.net.MediaType.JSON_UTF_8;
+import static org.asynchttpclient.Dsl.asyncHttpClient;
 
 public class ApplicationHandler implements Route {
 
@@ -32,6 +39,25 @@ public class ApplicationHandler implements Route {
 
     private CompletionStage<ResponseContainer> callRemoteServiceWithParameter(String id) {
 
-        return null;
+        LOG.info("Requesting information for Entity: [{}]", id);
+
+        CompletableFuture<User> userResponseFuture = asyncHttpClient().prepareGet("http://jsonplaceholder.typicode.com/users/" + id)
+                                                                      .execute()
+                                                                      .toCompletableFuture()
+                                                                      .thenApply(response -> {
+                                                                          LOG.info("parsing response from [{}, {}]", response.getUri(), Instant.now().toEpochMilli());
+                                                                          return (User) JsonSupport.jsonParser(response.getResponseBody(), User.class);
+                                                                      });
+
+        CompletableFuture<UserPost[]> postResponseFuture = asyncHttpClient().prepareGet("http://jsonplaceholder.typicode.com/posts?userId=" + id)
+                                                                            .execute()
+                                                                            .toCompletableFuture()
+                                                                            .thenApply(response -> {
+                                                                                LOG.info("parsing response from [{}, {}]", response.getUri(), Instant.now().toEpochMilli());
+                                                                                return (UserPost[]) JsonSupport.jsonParser(response.getResponseBody(), UserPost[].class);
+                                                                            });
+
+        return CompletableFutures.combine(userResponseFuture, postResponseFuture, ResponseContainer::new);
     }
+
 }
